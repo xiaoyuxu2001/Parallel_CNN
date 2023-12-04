76f258c15b28516ee3f9ae739eaab0eb14ec3cbe
xiaoyuxu2001 <92376636+xiaoyuxu2001@users.noreply.github.com>
2023-12-03 19:46:59 -0500

    Update dense.py

--- dense.py
+++ dense.py
@@ -1,10 +1,71 @@
 import numpy as np
+from typing import Tuple
+def shuffle(X: np.ndarray, y: np.ndarray, 
+            epoch: int) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    DO NOT modify this function.
 
+    Permute the training data for SGD.
+    :param X: The original input data in the order of the file.
+    :param y: The original labels in the order of the file.
+    :param epoch: The epoch number (0-indexed).
+    :return: Permuted X and y training data for the epoch.
+    """
+    np.random.seed(epoch)
+    N = len(y)
+    ordering = np.random.permutation(N)
+    return X[ordering], y[ordering]
 def flatten(input_array):
     """
     Reshapes the input array into a one-dimensional array.
     """
     return np.array(input_array.reshape(-1))
+
+def zero_init(shape : Tuple[int, int]) -> np.ndarray:
+    """
+    DO NOT modify this function.
+
+    ZERO Initialization: All weights are initialized to 0.
+
+    :param shape: list or tuple of shapes
+    :return: initialized weights
+    """
+    return np.zeros(shape=shape)
+
+
+def random_init(shape : Tuple[int, int]) -> np.ndarray:
+    """
+
+    RANDOM Initialization: The weights are initialized randomly from a uniform
+        distribution from -0.1 to 0.1.
+
+    :param shape: list or tuple of shapes
+    :return: initialized weights
+    """
+    M, D = shape
+    np.random.seed(M * D)  # Don't change this line!
+
+    # TODO: create the random matrix here!
+    # Hint: numpy might have some useful function for this
+    # Hint: make sure you have the right distribution
+    W = np.random.uniform(-0.1, 0.1, shape)
+    return W
+
+def shuffle(X: np.ndarray, y: np.ndarray, 
+            epoch: int) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    DO NOT modify this function.
+
+    Permute the training data for SGD.
+    :param X: The original input data in the order of the file.
+    :param y: The original labels in the order of the file.
+    :param epoch: The epoch number (0-indexed).
+    :return: Permuted X and y training data for the epoch.
+    """
+    np.random.seed(epoch)
+    N = len(y)
+    ordering = np.random.permutation(N)
+    return X[ordering], y[ordering]
 
 class Flatten:
     def forward(self, input):
@@ -44,6 +105,7 @@ class Relu:
         """
         self.last_input = input
         return np.maximum(0, input)
+      
     def d_relu(self, x):
           return 1 if x > 0 else 0
 
@@ -55,51 +117,63 @@ class Relu:
         d_L_d_input = np.where(d_L_d_input > 0, 1, 0)
         return d_L_d_input
 
-class Softmax:
-  # A standard fully-connected layer with softmax activation.
-
-  def __init__(self, input_len, nodes):
-    limit = np.sqrt(6 / (input_len + nodes))
-    self.weights = np.random.uniform(-limit, limit, size = (input_len, nodes))
-    self.biases = np.zeros(nodes)
     
-  def softmax(self, x):
-    return np.exp(x)/sum(np.exp(x))
+class SoftMaxCrossEntropy:
 
-  def forward(self, input):
-    '''
-    Performs a forward pass of the softmax
-    '''
-    self.last_input_shape = input.shape
+    def _softmax(self, z: np.ndarray) -> np.ndarray:
+        """
+        Implement softmax function.
+        :param z: input logits of shape (num_classes,)
+        :return: softmax output of shape (num_classes,)
+        """
+        # TODO: implement
+        sum_exp = np.sum(np.exp(z))
+        return np.exp(z) / sum_exp
 
-    input = flatten(input)
-    self.last_input = input
+    def _cross_entropy(self, y: int, y_hat: np.ndarray) -> float:
+        """
+        Compute cross entropy loss.
+        :param y: integer class label
+        :param y_hat: prediction with shape (num_classes,)
+        :return: cross entropy loss
+        """
+        # TODO: implement
+        # This is because we have 10 classes in total, if failed, that means my assumption is wrong
+        assert(len(y_hat) == 10)
+        assert(y >= 0 and y < 10)
+        
+        loss = -np.log(y_hat[y])
+        
+        return loss
 
-    totals = np.dot(input, self.weights) + self.biases
-    self.last_totals = totals
-    return self.softmax(totals)
+    def forward(self, z: np.ndarray, y: int) -> Tuple[np.ndarray, float]:
+        """
+        Compute softmax and cross entropy loss.
+        :param z: input logits of shape (num_classes,)
+        :param y: integer class label
+        :return:
+            y: predictions from softmax as an np.ndarrayt
+            loss: cross entropy loss
+        """
+        # TODO: Call your implementations of _softmax and _cross_entropy here
+        y_hat = self._softmax(z)
+        cross_entropy = self._cross_entropy(y, y_hat)
+        return y_hat, cross_entropy
 
-  def backprop(self, d_L_d_out, lr):
-    '''
-    Performs a backward pass of the softmax
-    '''
-    t_exp = np.exp(self.last_totals)
-    S = np.sum(t_exp)
-    d_t_d_w = self.last_input
-    d_t_d_b = 1
-    d_t_d_inputs = self.weights
-    
-    for i, gradient in enumerate(d_L_d_out):
-      if gradient == 0:
-        continue
+    def backprop(self, y: int, y_hat: np.ndarray) -> np.ndarray:
+        """
+        Compute gradient of loss w.r.t. ** softmax input **.
+        Note that here instead of calculating the gradient w.r.t. the softmax
+        output, we are directly computing gradient w.r.t. the softmax input.
 
-      d_out_d_t = -t_exp[i] * t_exp / (S ** 2)
-      d_out_d_t[i] = t_exp[i] * (S - t_exp[i]) / (S ** 2)
-      d_L_d_t = gradient * d_out_d_t
+        Try deriving the gradient yourself (Question 2.2(b) on the study guide),
+        and you'll see why we want to calculate this in a single step.
 
-      d_L_d_w =np.matmul(d_t_d_w[np.newaxis].T, d_L_d_t[np.newaxis])
-      d_L_d_b = d_L_d_t * d_t_d_b
-      d_L_d_inputs = np.matmul(d_t_d_inputs, d_L_d_t)
-      self.weights -= lr * d_L_d_w
-      self.biases -= lr * d_L_d_b
-      return d_L_d_inputs.reshape(self.last_input_shape)
+        :param y: integer class label
+        :param y_hat: predicted softmax probability with shape (num_classes,)
+        :return: gradient with shape (num_classes,)
+        """
+        # TODO: implement using the formula you derived in the written
+        res = y_hat.copy()
+        res[y] -= 1
+        return res
