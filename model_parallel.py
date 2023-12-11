fixed shape mismatch in parallel linear forward. gathered output is 3D, however, softmax only deal with 1d array

model_parallel.py
@@ -29,9 +29,13 @@ class Parallel_Linear:
 
     def forward(self, x: np.ndarray) -> np.ndarray:
         # Insert bias term
-        x = np.insert(x, 0, 1)
+        print(x.shape)
+        bias = np.ones((x.shape[0], 1))
+        print(x.shape)
+        x = np.hstack((x, bias))
+        print(x.shape)
         self.input = x
-        
+        print(self.local_w.T.shape)
         # Perform the local part of the forward pass
         local_y = np.dot(x, self.local_w.T)
         
@@ -43,6 +47,7 @@ class Parallel_Linear:
         self.comm.Gather(local_y, gathered_y, root=0)
         
         # Only the root process will have the complete output
+        print("gathered_y's shape: " + str(gathered_y.shape))
         return gathered_y
 
     def backward(self, dz: np.ndarray) -> np.ndarray:

parallel_cnn_model.py
@@ -30,17 +30,19 @@ class ParallelCNN:
 
     def forward(self, image, label, epoch):
         recvbuf = data_parallel_forward(image, comm, self)
-        out = recvbuf
+        out = recvbuf.reshape((1, -1))
         print(out.shape) #(20000,)
         for layer in self.dense_layers:
             out = layer.forward(out)
+            print(out.shape)
 
         # Gather outputs at root for softmax and loss
         gathered_output = None
         if rank == 0:
             gathered_output = np.empty([size, *out.shape], dtype=out.dtype)
-        
         comm.Gather(out, gathered_output, root=0)
+        print(out.shape)
+        print(gathered_output.shape)
 
         # Compute loss and softmax only on the root
         if rank == 0: